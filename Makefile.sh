30ce9969c7171ebd6b056f60ce1299febe7bd194
StubbornVegeta <2273893094@qq.com>
2022-08-13 17:21:08 +0800

    add TF_LFLAGS

--- Makefile.sh
+++ Makefile.sh
@@ -1,4 +1,4 @@
 TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
 TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
 
-g++ -std=c++11 -shared op_mymatmul.cc -o op_mymatmul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS} -O2
+g++ -std=c++11 -shared op_mymatmul.cc -o op_mymatmul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O2
